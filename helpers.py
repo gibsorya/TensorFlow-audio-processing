f14dfe4f630dbc76b01bf36ebc73a6dcea93a243
gibsorya <gibsorya@gmail.com>
2020-04-29 09:16:31 -0500

    Update to TF 2.0

--- helpers.py
+++ helpers.py
@@ -1,15 +1,19 @@
 """
-Authors:    Dario Cazzani
+Original Author:    Dario Cazzani
+
+Converted to TF 2.0 by Ryan Gibson 
 """
 #!/usr/bin/python
 from __future__ import division
-import tensorflow as tf
+import tensorflow_core as tf
 import numpy as np
 
+
 def log10(x):
-    num = tf.log(x)
-    den = tf.log(tf.constant(10, dtype=num.dtype))
-    return(tf.div(num, den))
+    num = tf.math.log(x)
+    den = tf.math.log(tf.constant(10, dtype=num.dtype))
+    return(tf.math.divide(num, den))
+
 
 def overlapping_slicer_3D(_input, block_size, stride):
     _input_rank = int(len(_input.get_shape()))
@@ -19,8 +23,9 @@ def overlapping_slicer_3D(_input, block_size, stride):
     high = range(block_size, n+1, stride)
     low_high = zip(low, high)
     for low, high in low_high:
-	blocks.append(_input[:, low:high])
+        blocks.append(_input[:, low:high])
     return(tf.stack(blocks, _input_rank-1))
+
 
 def angle(z):
     if z.dtype == tf.complex128:
@@ -30,13 +35,14 @@ def angle(z):
     else:
         raise ValueError('input z must be of type complex64 or complex128')
 
-    x = tf.real(z)
-    y = tf.imag(z)
+    x = tf.math.real(z)
+    y = tf.math.imag(z)
     x_neg = tf.cast(x < 0.0, dtype)
     y_neg = tf.cast(y < 0.0, dtype)
     y_pos = tf.cast(y >= 0.0, dtype)
     offset = x_neg * (y_pos - y_neg) * np.pi
-    return tf.atan(y / x) + offset
+    return tf.math.atan(y / x) + offset
+
 
 def is_power2(x):
     return x > 0 and ((x & (x - 1)) == 0)
